73d442c633514b2c4f1634b49be1919a7b5182b7
Damon <dong765@qq.com>
2023-12-22 15:26:31 +0800

    U

--- blog/js/utils.js
+++ blog/js/utils.js
@@ -601,20 +601,20 @@ const anzhiyu = {
     const $sayhiEl = document.getElementById("author-info__sayhi");
 
     const getTimeState = () => {
-      const hour = new Date().getHours();
-      let message = "";
+      // const hour = new Date().getHours();
+      let message = "IT Fisher";
 
-      if (hour >= 0 && hour <= 5) {
-        message = "睡个好觉，保证精力充沛";
-      } else if (hour > 5 && hour <= 10) {
-        message = "一日之计在于晨";
-      } else if (hour > 10 && hour <= 14) {
-        message = "吃饱了才有力气干活";
-      } else if (hour > 14 && hour <= 18) {
-        message = "集中精力，攻克难关";
-      } else if (hour > 18 && hour <= 24) {
-        message = "不要太劳累了，早睡更健康";
-      }
+      // if (hour >= 0 && hour <= 5) {
+      //   message = "睡个好觉，保证精力充沛";
+      // } else if (hour > 5 && hour <= 10) {
+      //   message = "一日之计在于晨";
+      // } else if (hour > 10 && hour <= 14) {
+      //   message = "吃饱了才有力气干活";
+      // } else if (hour > 14 && hour <= 18) {
+      //   message = "集中精力，攻克难关";
+      // } else if (hour > 18 && hour <= 24) {
+      //   message = "不要太劳累了，早睡更健康";
+      // }
 
       return message;
     };
